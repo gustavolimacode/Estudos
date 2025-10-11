4327bec1c237de179b2611e7d9ad2a4a3ebfa034
Gustavo Borges de Lima <gustavoblima306@gmail.com>
2025-10-11 13:01:11 -0300

    Estudando Pandas

--- Python/Pandas/introducao.py
+++ Python/Pandas/introducao.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Criando um DataFrame manualmente
 dados = [
     {"produto": 'Pizza', "valor": 30, "vendas": 300},
     {"produto": 'Cerveja', "valor": 10, "vendas": 320},
@@ -8,7 +9,9 @@ dados = [
 
 df = pd.DataFrame(dados)
 
+# Salvando o DataFrame em excel
 df.to_excel("dataframe_pandas.xlsx", index=False)
 
+# Lendo o arquivo excel
 new_df = pd.read_excel("dataframe_pandas.xlsx")
 print(new_df.head())
